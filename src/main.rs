d60f6c1b6f814d494b301b4df373a2cd7c2e5b41
dominic gasperini <dominicgasperini@gmail.com>
2023-03-27 13:00:47 -0400

    now produces 3 unique passphrases

--- src/main.rs
+++ src/main.rs
@@ -30,74 +30,109 @@ fn load_words() -> Vec<String>{
 }
 
 
-fn basic(words: &Vec<String>) -> String {
+fn basic(words: &Vec<String>) -> Vec<String> {
     // inits
+    let mut passphrases: Vec<String> = Vec::new();
     let mut passphrase = String::from("").to_owned();
     let splitter: &str = "-";
     
-    // get 3 random words
-    let phrases:Vec<&String> = words.choose_multiple(&mut rand::thread_rng(), 3).collect();
+    for _ in 0..3 {
+        // get 3 random words
+        let phrases:Vec<&String> = words.choose_multiple(&mut rand::thread_rng(), 3).collect();
 
-    passphrase.push_str(phrases[0].as_str());
-    passphrase.push_str(splitter);
-    passphrase.push_str(phrases[1].as_str());
-    passphrase.push_str(splitter);
-    passphrase.push_str(phrases[2].as_str());
+        passphrase.push_str(phrases[0].as_str());
+        passphrase.push_str(splitter);
+        passphrase.push_str(phrases[1].as_str());
+        passphrase.push_str(splitter);
+        passphrase.push_str(phrases[2].as_str());
 
-    // return passphrase
-    passphrase
+        passphrases.push(passphrase.clone());
+        passphrase.clear();
+    }
+
+    passphrases
 }
 
-fn moderate(words: &Vec<String>) -> String {
+fn moderate(words: &Vec<String>) -> Vec<String> {
     // inits
-    let mut passphrase = String::from("").to_owned();
+    let mut passphrases: Vec<String> = Vec::new();
+    let mut passphrase: String = String::from("").to_owned();
     let splitter: &str = "-";
-    let secret_number = rand::thread_rng().gen_range(1..=100);
-    
-    // get 3 random words
-    let phrases:Vec<&String> = words.choose_multiple(&mut rand::thread_rng(), 3).collect();
 
-    passphrase.push_str(phrases[0].as_str());
-    passphrase.push_str(splitter);
-    passphrase.push_str(phrases[1].as_str());
-    passphrase.push_str(splitter);
-    passphrase.push_str(phrases[2].as_str());
-    passphrase.push_str(secret_number.to_string().as_str());
+    for _ in 0..3 {
+        // get 3 random words
+        let phrases:Vec<&String> = words.choose_multiple(&mut rand::thread_rng(), 3).collect();
 
-    // return passphrase
-    passphrase
+        // get random number
+        let secret_number: i32 = rand::thread_rng().gen_range(1..=100);        
+
+        passphrase.push_str(phrases[0].as_str());
+        passphrase.push_str(secret_number.to_string().as_str());
+
+        passphrase.push_str(splitter);
+
+        let first_char = phrases[1].chars().next().unwrap().to_uppercase();
+        let capitalized = format!("{}{}", first_char, &phrases[1][1..]);
+        passphrase.push_str(capitalized.as_str());
+
+        passphrase.push_str(splitter);
+
+        passphrase.push_str(phrases[2].as_str());
+
+
+        // add to list of passphrases and reset
+        passphrases.push(passphrase.clone());
+        passphrase.clear();
+    } 
+
+    passphrases
 }
 
-fn advanced(words: &Vec<String>) -> String {
+fn advanced(words: &Vec<String>) -> Vec<String> {
     // inits
-    let mut passphrase = String::from("").to_owned();
+    let mut passphrases: Vec<String> = Vec::new();
+    let mut passphrase: String = String::from("").to_owned();
     let splitter: &str = "-";
-    let secret_number = rand::thread_rng().gen_range(1..=100);
-    let special_characters = vec!["!", "@", "#", "$", "&", "/"];
     
-    // get 3 random words
-    let phrases:Vec<&String> = words.choose_multiple(&mut rand::thread_rng(), 4).collect();
+    for _ in 0..3 {
+        // get 3 random words
+        let phrases:Vec<&String> = words.choose_multiple(&mut rand::thread_rng(), 4).collect();
+        
+        // get random number and special character
+        let secret_number: i32 = rand::thread_rng().gen_range(1..=100);
+        let special_characters = vec!["!", "@", "#", "$", "&", "/"];
 
-    passphrase.push_str(special_characters.choose(&mut rand::thread_rng()).unwrap());
-    passphrase.push_str(phrases[0].as_str());
-    passphrase.push_str(splitter);
-    passphrase.push_str(phrases[1].as_str());
-    passphrase.push_str(splitter);
-    passphrase.push_str(phrases[2].as_str());
-    passphrase.push_str(secret_number.to_string().as_str());
+        passphrase.push_str(phrases[0].as_str());
+        passphrase.push_str(secret_number.to_string().as_str());
 
-    // return passphrase
-    passphrase
+        passphrase.push_str(splitter);
+
+        let first_char = phrases[1].chars().next().unwrap().to_uppercase();
+        let capitalized = format!("{}{}", first_char, &phrases[1][1..]);
+        passphrase.push_str(capitalized.as_str());
+
+        passphrase.push_str(splitter);
+
+        passphrase.push_str(phrases[2].as_str());
+        passphrase.push_str(special_characters.choose(&mut rand::thread_rng()).unwrap());
+
+
+        // add to list of passphrases and reset
+        passphrases.push(passphrase.clone());
+        passphrase.clear();
+    }
+
+    passphrases
 }
 
 
 fn main() {
     // inits
-    let mut passphrase:String = String::new();
+    let mut passphrases:Vec<String> = Vec::new();
     let mut choice:String = String::new();
 
     // welcome
-    println!("welcome to passphrase generator!\n");
+    println!("\n---passphrase generator---\n");
     println!("select a complexity level: ");
     println!("1. basic      (3 random words)");
     println!("2. moderate   (3 random words + number)");
@@ -108,20 +143,24 @@ fn main() {
 
     // get user input
     io::stdin().read_line(&mut choice)
-        .ok()
-        .expect("Invalid input...");
+    .ok()
+    .expect("Invalid input...");
+
 
     // load in words!
     let words = load_words();
 
     match choice.trim() {
-        "1" => passphrase = basic(&words),
-        "2" => passphrase = moderate(&words),
-        "3" => passphrase = advanced(&words),
-        _ => println!("Invalid choice!"),
+        "1" => passphrases = basic(&words),
+        "2" => passphrases = moderate(&words),
+        "3" => passphrases = advanced(&words),
+        _ => println!("Invalid choice!")
     }
     
     // apply some color and print final passphrase
-    passphrase = passphrase.green().bold().to_string();
-    println!("\nNew passphrase: {passphrase}\n");
+    println!("\nPassphrases:");
+    for i in 0..3 {
+        println!("{}", passphrases.get(i).unwrap().green());
+    }
+    println!();
 }
